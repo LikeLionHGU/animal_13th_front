7c28445a5dffd77c70c1350b9a57f61705c1cdf2
Gyuho-Han <0203ho@naver.com>
2025-02-04 17:07:27 +0900

    init: 네비게이션 연결

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import FoundForm from './components/FoundForm';
+import LostForm from './components/LostForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import MyPage from './components/MyPage';
+import LostPage from './components/LostPage';
+import FoundPage from './components/FoundPage';
+import AlertPage from './components/AlertPage';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="/found-form" element={<FoundForm/>}/> 
+          <Route path="/lost-form" element={<LostForm/>}/>
+          <Route path="/mypage" element={<MyPage/>}/>
+          <Route path="/found-page" element={<FoundPage/>}/> 
+          <Route path="/lost-page" element={<LostPage/>}/>
+          <Route path="/alert-page" element={<AlertPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

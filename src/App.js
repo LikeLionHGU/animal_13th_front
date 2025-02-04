a94cffb3087712027af072c9e2d9d2a8c9cf952a
yean1234 <peaseoyean@gmail.com>
2025-02-04 17:44:24 +0900

    메인 머지

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Api from './components/Api';
+
+import LostForm from './components/LostForm';
+import MainPage from './components/MainPage';
+import MyPage from './components/MyPage';
+import LostPage from './components/LostPage';
+import FoundPage from './components/FoundPage';
+import AlertPage from './components/AlertPage';
+import FoundForm from './components/FoundForm';
+//메인에 머지~~
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="/found-form-map" element={<Api/>}/> 
+          <Route path="/found-form" element={<FoundForm/>}/> 
+          <Route path="/lost-form" element={<LostForm/>}/>
+          <Route path="/mypage" element={<MyPage/>}/>
+          <Route path="/found-page" element={<FoundPage/>}/> 
+          <Route path="/lost-page" element={<LostPage/>}/>
+          <Route path="/alert-page" element={<AlertPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

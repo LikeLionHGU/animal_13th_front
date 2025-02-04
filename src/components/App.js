1af6092c5844db3b8cf5a492a312c5a989fbe641
Gyuho-Han <0203ho@naver.com>
2025-02-04 14:42:06 +0900

    카메라 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Camera from './Camera';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Camera/>}/>
         </Routes>
       </BrowserRouter>
     </div>

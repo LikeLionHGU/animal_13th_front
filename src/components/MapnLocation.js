97d026ce3444135ee735496f53b1128de3e5b3fa
yean1234 <peaseoyean@gmail.com>
2025-02-05 16:27:41 +0900

    feat: 사용자 현재 위치

--- src/components/MapnLocation.js
+++ src/components/MapnLocation.js
@@ -1,17 +1,35 @@
 
  
+import { useEffect, useState } from "react";
 import { Container as MapDiv, NaverMap, useNavermaps, Marker } from "react-naver-maps";
 
 function MapnLocation() {
   const navermaps = useNavermaps();
+  const [location, setLocation] = useState(null); //처음 유저 정보
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLocation(new navermaps.LatLng(latitude, longitude)); // 위치 상태 업데이트
+        },
+        (error) => {
+          console.error("Geolocation error:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, [navermaps]);
 
   return (
     <MapDiv style={{ width: "100%", height: "100%" }}>
       <NaverMap
         zoom={10}
-        center={new navermaps.LatLng(37.56667, 126.97806)}
+        center={location || new navermaps.LatLng(37.56667, 126.97806)} // 위치가 없으면 기본값
       >
-        <Marker position={new navermaps.LatLng(37.56667, 126.97806)} />
+        {location && <Marker position={location} />} 
       </NaverMap>
     </MapDiv>
   );

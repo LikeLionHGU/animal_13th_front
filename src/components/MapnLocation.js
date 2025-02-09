0fb115c4aa1d620a29bed6e15a5276d3b87807b4
yean1234 <peaseoyean@gmail.com>
2025-02-09 17:51:22 +0900

    feat: 지도 로딩

--- src/components/MapnLocation.js
+++ src/components/MapnLocation.js
@@ -7,6 +7,8 @@ import { Container as MapDiv, NaverMap, useNavermaps, Marker } from "react-naver
 function MapnLocation() {
   const navermaps = useNavermaps();
   const [location, setLocation] = useState(null); //처음 유저 정보
+  const [zoom, setZoom] = useState(10);
+  const [load, setLoad] = useState(0);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -14,6 +16,8 @@ function MapnLocation() {
         (position) => {
           const { latitude, longitude } = position.coords;
           setLocation(new navermaps.LatLng(latitude, longitude)); // 위치 상태 업데이트
+          setZoom(30);
+          setLoad(1);
         }
       );
     } 
@@ -21,12 +25,12 @@ function MapnLocation() {
 
   return (
     <MapDiv style={{ width: "100%", height: "100%" }}>
-      <NaverMap
-        zoom={10}
-        center={location || new navermaps.LatLng(37.56667, 126.97806)} // 위치가 없으면 기본값
+      {load === 0? "loading...": <NaverMap
+        zoom={zoom}
+        center={location|| new navermaps.LatLng(37.56667, 126.97806)} // 위치가 없으면 기본값
       >
         {location && <Marker position={location} />} 
-      </NaverMap>
+      </NaverMap>}
     </MapDiv>
   );
 }

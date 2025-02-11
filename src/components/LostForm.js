347d29329a78487716e4858d4fab8f163aa7d5d5
Gyuho-Han <0203ho@naver.com>
2025-02-11 21:11:42 +0900

    commit

--- src/components/LostForm.js
+++ src/components/LostForm.js
@@ -79,12 +79,13 @@ const LostForm = () => {
           <select name="category" id="category" className={styles.formField} style={{cursor: "pointer"}} required>
             <option value="">카테고리</option>
             <option value="1">전자기기</option>
-            <option value="2">카드/지갑/현금</option>
-            <option value="3">택배</option>
-            <option value="4">도서 및 서류</option>
-            <option value="5">의류/액세서리</option>
-            <option value="6">가방</option>
-            <option value="7">기타</option>
+            <option value="2">카드/학생증</option>
+            <option value="3">지갑/현금</option>
+            <option value="4">택배</option>
+            <option value="5">도서 및 서류</option>
+            <option value="6">의류/액세서리</option>
+            <option value="7">가방</option>
+            <option value="8">기타</option>
           </select>
           {/* <label htmlFor="category" className={styles.formLabel}>카테고리</label> */}
         </div>

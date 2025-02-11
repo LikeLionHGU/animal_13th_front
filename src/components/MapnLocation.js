3fb1ece1d854eb147d32c30a6f2a672d9073b08c
Gyuho-Han <0203ho@naver.com>
2025-02-11 10:50:45 +0900

    style: found글 작성 페이지 css 적용

--- src/components/MapnLocation.js
+++ src/components/MapnLocation.js
@@ -7,31 +7,34 @@ function MapnLocation({ setLocation }) {
   const navermaps = useNavermaps();
   const [mapLocation, setMapLocation] = useState(null);
 
-
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const newLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
+        async (position) => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+          const newLocation = { lat, lng };
+
           setMapLocation(newLocation);
           setLocation(newLocation);
-          setLoad(1);
+          setLoad(true);
+
         },
         (error) => {
           console.error("Geolocation error:", error);
+          setLoad(true); // 에커 발생시에도 로딩 중단
         }
       );
     } else {
       console.error("Geolocation is not supported by this browser.");
     }
   }, [setLocation]);
-
-
+  
   const handleMarker = (e) => {
     const lat = e.coord.lat();
     const lng = e.coord.lng();
-    setMapLocation(new navermaps.LatLng(lat, lng));
-    setLocation(new navermaps.LatLng(lat, lng));
+    setMapLocation({ lat, lng });
+    setLocation({ lat, lng });
     console.log(lat, lng);
   };
 
@@ -39,7 +42,7 @@ function MapnLocation({ setLocation }) {
     <MapDiv style={{ width: "100%", height: "100%" }}>
       {load === 0? "loading...": <NaverMap
         zoom={19}
-        center={mapLocation || { lat: 36.08333, lng: 129.36667 }} // 기본값: 포항시
+        center={mapLocation || { lat: 36.103096, lng: 129.387299 }} // 기본값: 한동대
         onClick={handleMarker}
       >
         {mapLocation && <Marker position={mapLocation} />}

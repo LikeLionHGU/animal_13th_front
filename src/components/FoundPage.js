ae1404c104e081ec4a5af73fd78c33568cd16495
Gyuho-Han <0203ho@naver.com>
2025-02-13 16:18:41 +0900

    style: 게시판 css 작업, 코드정리

--- src/components/FoundPage.js
+++ src/components/FoundPage.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import styles from '../styles/Page.module.css'
+import { ReactComponent as Logo } from "../assets/icons/zuumLogo.svg"; // ReactComponent로 불러오기
+import { ReactComponent as WriteIcon } from "../assets/icons/writeIcon.svg"; // ReactComponent로 불러오기
 
 function FoundPage() {
   return (
     <div>
-      <h1>Found 게시판</h1>
+      <div className={styles.zummLogoContainer}>
+        <Logo className={styles.zuumLogo}/>
+      </div>
+      <div className={styles.title}>
+        <span className={styles.Lost}>FOUND</span> 
+        <span className={styles.comma}>, </span>
+        <span className={styles.restTitle}>물건을 찾았어요</span>
+      </div>
+      <div className={styles.intro}>혹시 잃어버린 물건이 있나요? 여기에서 확인해보세요!</div>
+      <div className={styles.writeIcon}>
+        <WriteIcon/>
+      </div>
     </div>
   )
 }
---
+++ src/components/FoundPageDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FoundPageDetail() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FoundPageDetail

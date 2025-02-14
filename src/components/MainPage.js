565d424ae518a5de439824b10e970a50fef409e6
Gyuho-Han <0203ho@naver.com>
2025-02-15 01:07:25 +0900

    edit: setFoundMain 코드 수정

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -33,7 +33,7 @@ function MainPage() {
           try {
                 const response = await axios.get("https://koyangyee.info/board/found/main");
                 console.log("Found: ", response.data.board);
-                setFoundMain(response.data);
+                setFoundMain(response.data.board);
           } catch (error) {
                 console.error("오류 발생:", error);
           } finally {

33294fe7a5688ad0180b08c62aaf5d4116f65a94
Gyuho-Han <0203ho@naver.com>
2025-02-16 18:27:42 +0900

    edit: foundform 신청 완료알림 수정

--- src/components/FoundForm.js
+++ src/components/FoundForm.js
@@ -99,9 +99,9 @@ const FoundForm = () => {
         withCredentials: true, 
       });
 
-    const { isLogin, isSuccess } = response.data;
+    const {isSuccess } = response.data;
 
-    if (isLogin === 1 && isSuccess === 1) {
+    if (isSuccess === 1) {
       console.log("업로드 완료:", response.data);
       alert("업로드 완료");
       navigate("/"); 

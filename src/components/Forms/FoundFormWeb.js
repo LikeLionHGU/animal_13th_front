3b8fcc08ea89e7e0d0a00d03453e5793208c6696
yean1234 <peaseoyean@gmail.com>
2025-02-17 11:52:20 +0900

    edit: request 수정

--- src/components/Forms/FoundFormWeb.js
+++ src/components/Forms/FoundFormWeb.js
@@ -56,7 +56,7 @@ const FoundForm = () => {
       try {
             const response = await axios.get("https://koyangyee.info/board/found/all/category/new", 
               {
-                params: { category: selectCategory } 
+               category: selectCategory 
               }
             );
             console.log("Lost: ", response.data.board);

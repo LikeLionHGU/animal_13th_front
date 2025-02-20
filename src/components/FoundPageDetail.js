2df93400fd9787481ab1d1782b6bd9dd59e7b9e5
Gyuho-Han <0203ho@naver.com>
2025-02-21 00:25:59 +0900

    edit: 오류 수정

--- src/components/FoundPageDetail.js
+++ src/components/FoundPageDetail.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useNavigate } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import styles from '../styles/FoundDetail.module.css';
 
